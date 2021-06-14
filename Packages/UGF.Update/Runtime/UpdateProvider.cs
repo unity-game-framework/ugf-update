8f57b1f5d2e9b44c33e7995b338f0a28f4f9d8e8
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-06-14 19:37:24 +0300

    Change update provider

--- Packages/UGF.Update/Runtime/UpdateProvider.cs
+++ Packages/UGF.Update/Runtime/UpdateProvider.cs
@@ -1,141 +1,44 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using UnityEngine.LowLevel;
+using UGF.RuntimeTools.Runtime.Providers;
 
 namespace UGF.Update.Runtime
 {
-    /// <summary>
-    /// Represents default implementation of the update provider.
-    /// </summary>
-    public class UpdateProvider : IUpdateProvider
+    public class UpdateProvider : Provider<IUpdateGroup, UpdateGroupFunction>, IUpdateProvider
     {
         public IUpdateLoop UpdateLoop { get; }
-        public IReadOnlyDictionary<string, IUpdateGroup> Groups { get; }
 
-        private readonly Dictionary<string, IUpdateGroup> m_groups = new Dictionary<string, IUpdateGroup>();
-        private readonly Dictionary<string, GroupInfo> m_infos = new Dictionary<string, GroupInfo>();
-
-        private readonly struct GroupInfo
-        {
-            public Type SubSystemType { get; }
-            public PlayerLoopSystem.UpdateFunction UpdateFunction { get; }
-
-            public GroupInfo(Type subSystemType, PlayerLoopSystem.UpdateFunction updateFunction)
-            {
-                SubSystemType = subSystemType ?? throw new ArgumentNullException(nameof(subSystemType));
-                UpdateFunction = updateFunction ?? throw new ArgumentNullException(nameof(updateFunction));
-            }
-        }
-
-        /// <summary>
-        /// Creates provider with default Unity update loop.
-        /// </summary>
-        public UpdateProvider() : this(new UpdateLoopUnity())
-        {
-        }
-
-        /// <summary>
-        /// Creates provider with the specified update loop.
-        /// </summary>
-        /// <param name="updateLoop">The update loop to use.</param>
         public UpdateProvider(IUpdateLoop updateLoop)
         {
             UpdateLoop = updateLoop ?? throw new ArgumentNullException(nameof(updateLoop));
-            Groups = new ReadOnlyDictionary<string, IUpdateGroup>(m_groups);
         }
 
-        public void Add(Type subSystemType, IUpdateGroup updateGroup)
+        protected override void OnAdd(IUpdateGroup id, UpdateGroupFunction entry)
         {
-            if (subSystemType == null) throw new ArgumentNullException(nameof(subSystemType));
-            if (updateGroup == null) throw new ArgumentNullException(nameof(updateGroup));
-            if (m_groups.ContainsKey(updateGroup.Name)) throw new ArgumentException($"A group with the same name already exists: '{updateGroup.Name}'.", nameof(updateGroup));
+            base.OnAdd(id, entry);
 
-            var info = new GroupInfo(subSystemType, updateGroup.Update);
-
-            UpdateLoop.AddFunction(subSystemType, info.UpdateFunction);
-
-            m_groups.Add(updateGroup.Name, updateGroup);
-            m_infos.Add(updateGroup.Name, info);
+            UpdateLoop.AddFunction(entry.SubSystemType, entry.UpdateFunction);
         }
 
-        public void Remove(IUpdateGroup updateGroup)
+        protected override bool OnRemove(IUpdateGroup id, UpdateGroupFunction entry)
         {
-            if (updateGroup == null) throw new ArgumentNullException(nameof(updateGroup));
-
-            Remove(updateGroup.Name);
-        }
-
-        public void Remove(string groupName)
-        {
-            if (string.IsNullOrEmpty(groupName)) throw new ArgumentException("Value cannot be null or empty.", nameof(groupName));
-
-            if (m_groups.Remove(groupName))
+            if (base.OnRemove(id, entry))
             {
-                GroupInfo info = m_infos[groupName];
-
-                UpdateLoop.RemoveFunction(info.SubSystemType, info.UpdateFunction);
-
-                m_infos.Remove(groupName);
+                UpdateLoop.RemoveFunction(entry.SubSystemType, entry.UpdateFunction);
+                return true;
             }
+
+            return false;
         }
 
-        public void Clear()
+        protected override void OnClear()
         {
-            foreach (KeyValuePair<string, GroupInfo> pair in m_infos)
+            foreach (KeyValuePair<IUpdateGroup, UpdateGroupFunction> pair in this)
             {
                 UpdateLoop.RemoveFunction(pair.Value.SubSystemType, pair.Value.UpdateFunction);
             }
 
-            m_groups.Clear();
-            m_infos.Clear();
-        }
-
-        public Type GetSubSystemType(string groupName)
-        {
-            if (string.IsNullOrEmpty(groupName)) throw new ArgumentException("Value cannot be null or empty.", nameof(groupName));
-
-            return TryGetSubSystemType(groupName, out Type type) ? type : throw new ArgumentException($"Group not found by the specified name: '{groupName}'.", nameof(groupName));
-        }
-
-        public bool TryGetSubSystemType(string groupName, out Type type)
-        {
-            if (string.IsNullOrEmpty(groupName)) throw new ArgumentException("Value cannot be null or empty.", nameof(groupName));
-
-            if (m_infos.TryGetValue(groupName, out GroupInfo info))
-            {
-                type = info.SubSystemType;
-                return true;
-            }
-
-            type = null;
-            return false;
-        }
-
-        public T GetGroup<T>(string groupName) where T : IUpdateGroup
-        {
-            if (string.IsNullOrEmpty(groupName)) throw new ArgumentException("Value cannot be null or empty.", nameof(groupName));
-
-            return TryGetGroup(groupName, out T group) ? group : throw new ArgumentException($"Group not found by the specified name: '{group}'.");
-        }
-
-        public bool TryGetGroup<T>(string groupName, out T updateGroup) where T : IUpdateGroup
-        {
-            if (string.IsNullOrEmpty(groupName)) throw new ArgumentException("Value cannot be null or empty.", nameof(groupName));
-
-            if (m_groups.TryGetValue(groupName, out IUpdateGroup value))
-            {
-                updateGroup = (T)value;
-                return true;
-            }
-
-            updateGroup = default;
-            return false;
-        }
-
-        public Dictionary<string, IUpdateGroup>.Enumerator GetEnumerator()
-        {
-            return m_groups.GetEnumerator();
+            base.OnClear();
         }
     }
 }
---
+++ Packages/UGF.Update/Runtime/UpdateProviderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UGF.Update.Runtime
+{
+    public static class UpdateProviderExtensions
+    {
+        public static void Add(this IUpdateProvider provider, Type subSystemType, IUpdateGroup updateGroup)
+        {
+            if (provider == null) throw new ArgumentNullException(nameof(provider));
+            if (subSystemType == null) throw new ArgumentNullException(nameof(subSystemType));
+            if (updateGroup == null) throw new ArgumentNullException(nameof(updateGroup));
+
+            var function = new UpdateGroupFunction(subSystemType, updateGroup.Update);
+
+            provider.Add(updateGroup, function);
+        }
+    }
+}
